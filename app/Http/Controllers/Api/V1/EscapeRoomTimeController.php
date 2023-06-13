738cac5b11787bce0e8715c42a4e847bb586bec6
magerishere <immagerishere@gmail.com>
2023-06-13 16:43:41 +0330

    make escape room time (Model,migrations,controller) and move controller to api v1

---
+++ app/Http/Controllers/Api/V1/EscapeRoomTimeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\EscapeRoomTime;
+use Illuminate\Http\Request;
+
+class EscapeRoomTimeController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EscapeRoomTime $escapeRoomTime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EscapeRoomTime $escapeRoomTime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EscapeRoomTime $escapeRoomTime)
+    {
+        //
+    }
+}
