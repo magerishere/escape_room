3d8b7f8e050a847a94574b7cec2a0f0fe48256b4
magerishere <immagerishere@gmail.com>
2023-06-13 21:13:27 +0330

    add return type static method

--- app/Facades/EscapeRoomTheme/EscapeRoomThemeFacade.php
+++ app/Facades/EscapeRoomTheme/EscapeRoomThemeFacade.php
@@ -2,10 +2,11 @@
 
 namespace App\Facades\EscapeRoomTheme;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static getAll
+ * @method static Collection getAll
  */
 class EscapeRoomThemeFacade extends Facade
 {
