e195faf8e1a95554e4c9a87bf390caabb1e5d526
magerishere <immagerishere@gmail.com>
2023-06-14 10:19:36 +0330

    modify booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,9 +9,24 @@ class Booking extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'price',
+        'discount_price',
+    ];
+
+    protected $casts = [
+        'price' => 'int',
+        'discount_price' => 'int'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function escapeRoomTime()
+    {
+        return $this->belongsTo(EscapeRoomTime::class);
     }
 
 }
