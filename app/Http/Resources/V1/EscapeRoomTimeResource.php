ae8fd6f3c15631ad0600b2f72cf177355626ae38
magerishere <immagerishere@gmail.com>
2023-06-14 13:18:59 +0330

    add remaining capacity escape room time in api resource

--- app/Http/Resources/V1/EscapeRoomTimeResource.php
+++ app/Http/Resources/V1/EscapeRoomTimeResource.php
@@ -15,9 +15,12 @@ class EscapeRoomTimeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
+            'capacity' => $this->capacity,
+            'remaining_capacity' => $this->remainingCapacity,
             'begin' => $this->begin->format('H:i:s'),
             'end' => $this->end->format('H:i:s'),
-            'price' => $this->price
+            'price' => $this->price,
         ];
     }
 }
--- app/Models/EscapeRoomTime.php
+++ app/Models/EscapeRoomTime.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EscapeRoomTime extends Model
@@ -32,17 +33,26 @@ class EscapeRoomTime extends Model
         return $this->belongsTo(EscapeRoomDate::class, 'escape_room_date_id', 'id');
     }
 
-    public function books()
+    public function books(): HasMany
     {
         return $this->hasMany(Booking::class);
+    }
+
+    public function remainingCapacity(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->capacity - $this->books()->count(),
+        );
     }
 
     public function isFulled(): Attribute
     {
         return Attribute::make(
-            get: fn(): bool => $this->books()->count() >= $this->capacity
+            get: fn(): bool => $this->remainingCapacity <= 0
         );
     }
+
+
 
 
 }
