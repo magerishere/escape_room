5c8a1b921b7e204463fda7cbae913bbc9b465d2d
magerishere <immagerishere@gmail.com>
2023-06-14 10:01:13 +0330

    add items relation in booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -13,4 +13,9 @@ class Booking extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function items()
+    {
+        return $this->hasMany(BookingItems::class);
+    }
 }
