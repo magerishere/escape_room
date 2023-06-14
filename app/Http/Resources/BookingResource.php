49d93bfd8543b53f28681427d690482b043649be
magerishere <immagerishere@gmail.com>
2023-06-14 11:38:39 +0330

    remove log alert

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -5,7 +5,6 @@ namespace App\Http\Resources;
 use App\Http\Resources\V1\EscapeRoomTimeResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Log;
 
 class BookingResource extends JsonResource
 {
@@ -16,7 +15,6 @@ class BookingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        Log::alert($this->time);
         return [
             'id' => $this->id,
             'price' => $this->price,
