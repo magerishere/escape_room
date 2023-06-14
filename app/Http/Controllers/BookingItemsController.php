a591cd2596346218bff734c2a1a4519f157003dd
magerishere <immagerishere@gmail.com>
2023-06-14 09:58:04 +0330

    create booking items (migration,model,controller)

---
+++ app/Http/Controllers/BookingItemsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Api\V1\ApiController;
+use App\Models\BookingItems;
+use Illuminate\Http\Request;
+
+class BookingItemsController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookingItems $bookingItems)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BookingItems $bookingItems)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookingItems $bookingItems)
+    {
+        //
+    }
+}
