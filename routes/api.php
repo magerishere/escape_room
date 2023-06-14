9a842b641dec7596625b8e06361ebb98aa800bbc
magerishere <immagerishere@gmail.com>
2023-06-14 08:00:36 +0330

    add login route as post method

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\EscapeRoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,11 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/login',);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::prefix('v1')->as('v1.')->group(function () {
+    // Authenticate
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    // Escape Rooms
     Route::apiResource('escape-rooms', EscapeRoomController::class)->only(['index', 'show']);
-    Route::get('escape-rooms/{escape_room}/time-slots', [EscapeRoomController::class, 'showTimes']);
+    Route::get('escape-rooms/{escape_room}/time-slots', [EscapeRoomController::class, 'showTimes'])->name('escape-rooms.showTimes');
 });
